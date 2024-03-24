7d76c27a2e29766cefc31261e00bf28d7535b29b
Bry_son <brayson@Braysons-MacBook-Pro.local>
2024-03-24 17:52:13 +0300

    assign1.dart

---
+++ assign1.dart
@@ -0,0 +1,18 @@
+
+void main() {
+    // Prompt the user for a number
+    print("Enter a number: ");
+    // check if the input is a valid number
+    if (15 !=null) {
+        // compare the number with 10 and print the message accordingly
+        if (15 > 10) {
+            print("Your number is greater than 10");
+        } else if (15 < 10) {
+            print("Your number is less than 10");
+        } else {
+            print("Your number is equal to 10");
+        }
+    } else {
+        print("Invalid input. Please enter a valid number.");
+    }
+}
